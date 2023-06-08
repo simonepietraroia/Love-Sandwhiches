5fb4d496cc7e6be9c96da3bcae60de50e108fff3
simonepietraroia <>
2023-06-08 15:57:23 +0000

    some message

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -22,7 +21,7 @@ def get_sales_data():
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
     
         sales_data = data_str.split(",")
         
@@ -71,7 +70,7 @@ def calculate_surplus_data(sales_row):
 def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_worksheet(sales_data. "sales")
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
     update_worksheet(new_surplus_data, "surplus")
 
